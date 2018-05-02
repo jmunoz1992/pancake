3d7ce1560520acb6173371a0e3f7dff6c576d70a
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-05-02 15:17:20 -0500

    completed login and initial octokit stuff

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>PANCAKE</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -16,17 +16,11 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
             Logout
           </a>
         </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
+      ) : null}
     </nav>
     <hr />
   </div>
-)
+);
 
 /**
  * CONTAINER
@@ -34,18 +28,18 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     handleClick() {
-      dispatch(logout())
+      dispatch(logout());
     }
-  }
-}
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navbar);
 
 /**
  * PROP TYPES
@@ -53,4 +47,4 @@ export default connect(mapState, mapDispatch)(Navbar)
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
---
+++ server/auth/github.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const passport = require("passport");
+const GitHubStrategy = require("passport-github").Strategy;
+const { User } = require("../db/models");
+const octokit = require("@octokit/rest")();
+
+const githubCredentials = {
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.GITHUB_CALLBACK
+};
+
+function verificationCallback(token, refreshToken, profile, done) {
+  // github may not provide an email, if so we'll just fake it
+  const username = profile.username;
+
+  const info = {
+    username,
+    token
+  };
+
+  User.findOrCreate({
+    where: { githubId: profile.id },
+    defaults: info
+  })
+    .spread(user => {
+      done(null, user);
+    })
+    .catch(done);
+}
+
+passport.use(new GitHubStrategy(githubCredentials, verificationCallback));
+
+router.get("/", passport.authenticate("github", { scope: ["repo", "admin:repo_hook", "gist"] }));
+
+router.get("/verify", passport.authenticate("github", { failureRedirect: "/login" }), (req, res) => {
+  res.redirect(`/users/${req.user.id}`);
+});
+
+module.exports = router;
