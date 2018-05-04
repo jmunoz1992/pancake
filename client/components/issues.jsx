747ba4c46888cbab192585e16510fecc71c40153
jmunoz1992 <jasmine.esplago.munoz@gmail.com>
2018-05-04 15:58:54 -0500

    updated issues stuff

--- client/components/issues.jsx
+++ client/components/issues.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import {} from "../store";
+import { Card, Icon, Label } from "semantic-ui-react";
 
 const Issues = props => {
   const { issues } = props;
@@ -10,7 +11,31 @@ const Issues = props => {
     <div>
       <h1>Issues</h1>
       {issues.map(issue => {
-        console.log("issue ", issue);
+        return (
+          <Card key={issue.id}>
+            <Card.Content header={issue.title} />
+            <Card.Content description={issue.body} />
+            <Card.Content extra>
+              {issue.labels.map(label => {
+                return (
+                  <Label as="a" tag key={label.id} style={{ backgroundColor: `#${label.color}` }}>
+                    {label.name}
+                  </Label>
+                );
+              })}
+            </Card.Content>
+            <Card.Content extra>
+              {issue.assignees.map(assignee => {
+                return (
+                  <Label as="a" image key={assignee.id}>
+                    <img src={assignee.avatar_url} />
+                    {assignee.login}
+                  </Label>
+                );
+              })}
+            </Card.Content>
+          </Card>
+        );
       })}
     </div>
   );
--- server/api/issues.js
+++ server/api/issues.js
@@ -13,3 +13,51 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
+
+/**
+ * post new issue into database
+ * need to use assignee method for issue
+ */
+router.post("/", async (req, res, next) => {
+  try {
+    // returns created issue object
+    const response = await req.octokit.issues.create({
+      owner: req.repoOwner,
+      repo: req.repoName,
+      title: req.body.title,
+      body: req.body.body,
+      assignees: req.body.assignees,
+      labels: req.body.labels
+    });
+    const issue = await Issue.create({
+      number: response.data.number,
+      projectId: req.user.activeProjectId
+    });
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
+ * edits new issue into database
+ * need to use assignee method for issue
+ */
+router.put("/:number", async (req, res, next) => {
+  try {
+    // returns created issue object
+    const response = await req.octokit.issues.edit({
+      owner: req.repoOwner,
+      repo: req.repoName,
+      number: req.params.number,
+      title: req.body.title,
+      body: req.body.body,
+      assignees: req.body.assignees,
+      labels: req.body.labels,
+      state: req.body.state
+    });
+    res.json(response.data);
+  } catch (err) {
+    next(err);
+  }
+});
