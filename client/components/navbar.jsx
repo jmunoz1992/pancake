bb4a50c8456d65b8e3067c0895bb10ded7ddcdbb
Victor Barrancos <victorbarrancos@gmail.com>
2018-05-04 17:02:24 -0500

    Added logo and cleaned up navbar

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,8 +6,19 @@ import { Menu, Header } from "semantic-ui-react";
 
 const Navbar = ({ handleClick }) => (
   <Menu fixed="top" inverted size="huge" borderless fluid>
-    <Menu.Item>Pancake</Menu.Item>
-    <Menu.Item as="a" onClick={handleClick}>
+    <Menu.Item>
+      <img src="/logo.png" /> Pancake
+    </Menu.Item>
+    <Menu.Item as="a" name="wireframe" onClick={handleClick}>
+      Wireframe
+    </Menu.Item>
+    <Menu.Item as="a" name="schema" onClick={handleClick}>
+      Schema
+    </Menu.Item>
+    <Menu.Item as="a" name="test" onClick={handleClick}>
+      Test
+    </Menu.Item>
+    <Menu.Item position="right" name="logout" as="a" onClick={handleClick}>
       Logout
     </Menu.Item>
   </Menu>
@@ -15,8 +26,16 @@ const Navbar = ({ handleClick }) => (
 
 const mapDispatch = dispatch => {
   return {
-    handleClick() {
-      dispatch(logout());
+    handleClick(_, { name }) {
+      switch (name) {
+        case "wireframe":
+          break;
+        case "schema":
+          break;
+        case "logout":
+          dispatch(logout());
+          break;
+      }
     }
   };
 };
