40b808076b8dc3e53e05c196342475e7393c2fc7
Victor Barrancos <victorbarrancos@gmail.com>
2018-05-11 08:22:46 -0500

    Removed a .log

--- client/components/designer/canvas.jsx
+++ client/components/designer/canvas.jsx
@@ -151,7 +151,6 @@ class DesignerCanvas extends Component {
       x: box.left + box.width - this.state.panOffsetX,
       y: box.top + box.height - this.state.panOffsetY
     };
-    console.log("Box:", boxLeft, boxRight);
     const selectedElements = [];
     this.props.elements.forEach(element => {
       const elementLeft = { x: element.left, y: element.top };
