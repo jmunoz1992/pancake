8153997acb3cc6b681a52305b700ee72ebb9a4a0
livilammister <livilammister@gmail.com>
2018-05-13 15:56:37 -0500

    adds cards to lanes based on tag

--- client/components/kanban-board/kanban-board.jsx
+++ client/components/kanban-board/kanban-board.jsx
@@ -5,10 +5,21 @@ import Board from "react-trello";
 class KanbanBoard extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            inboxIssues: [],
+        };
     }
 
-    getData = () => {
+    createIssueCard = issue => {
+        return {
+            id: issue.id,
+            title: issue.title,
+            description: issue.body,
+        };
+    }
+
+    createBoard = () => {
+        const { inboxIssues, nextIssues, todoIssues, inProgressIssues, completedIssues } = this.props;
         return {
             lanes:
                 [
@@ -16,31 +27,31 @@ class KanbanBoard extends Component {
                         id: "lane1",
                         title: "Inbox",
                         label: "0/0",
-                        cards: []
+                        cards: inboxIssues ? inboxIssues.map(issue => this.createIssueCard(issue)) : [],
                     },
                     {
                         id: "lane2",
                         title: "To-Do",
                         label: "0/0",
-                        cards: []
+                        cards: todoIssues ? todoIssues.map(issue => this.createIssueCard(issue)) : [],
                     },
                     {
                         id: "lane3",
                         title: "Next",
                         label: "0/0",
-                        cards: []
+                        cards: nextIssues ? nextIssues.map(issue => this.createIssueCard(issue)) : [],
                     },
                     {
                         id: "lane4",
                         title: "In Progress",
                         label: "0/0",
-                        cards: []
+                        cards: inProgressIssues ? inProgressIssues.map(issue => this.createIssueCard(issue)) : []
                     },
                     {
                         id: "lane5",
                         title: "Completed",
                         label: "0/0",
-                        cards: []
+                        cards: completedIssues ? completedIssues.map(issue => this.createIssueCard(issue)) : []
                     },
                 ]
         };
@@ -49,12 +60,22 @@ class KanbanBoard extends Component {
     render() {
         return (
             <div>
-                <Board data={this.getData()} />
+                <Board data={this.createBoard()} />
             </div>
         );
     }
 }
 
-const mapState = ({ issues, collaborators, labels }) => ({ issues, collaborators, labels });
+const mapState = ({ issues, collaborators, labels }) => {
+    const issueList = issues.issueList;
+    const todoIssues = issueList.filter(issue => issue.labels.find(label => label.name === "todo"));
+    const nextIssues = issueList.filter(issue => issue.labels.find(label => label.name === "next"));
+    const inProgressIssues = issueList.filter(issue => issue.labels.find(label => label.name === "in progress"));
+    const completedIssues = issueList.filter(issue => issue.labels.find(label => label.name === "completed"));
+    // issues missing a lane tag go in the inbox
+    const inboxIssues = issueList.filter(issue => issue.labels.find(label => (label.name !== "todo") && (label.name !== "next") && (label.name !== "in progress") && (label.name !== "completed")));
+    console.log("inboxIssues", inboxIssues);
+    return { collaborators, labels, todoIssues, nextIssues, inProgressIssues, completedIssues };
+};
 
 export default connect(mapState, null)(KanbanBoard);
